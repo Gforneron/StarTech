648c41022e1a922a98db43a8aa65dafccf5b8a87
Gforneron <gonzaloforneron12@gmail.com>
2023-02-13 12:16:49 -0300

    Creacion sistema de carpetas

---
+++ Sprint 2/app.js
@@ -0,0 +1,20 @@
+// LLamadas de Librerias
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.port || 8080;
+
+// declaracion y uso de recursos de public
+
+const publico = path.resolve(__dirname, "./public");
+app.use(express.static(publico));
+
+// Levantar servidor
+
+app.listen(port, () => console.log("Corriendo servidor"));
+
+// Subir html
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
