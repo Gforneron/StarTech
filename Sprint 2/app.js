992fdb491a8d6680924ba228723526177207e03d
MaxiDeV1 <maximovelasquez411@gmail.com>
2023-02-17 22:53:06 -0300

    subiendo login y register

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -18,7 +18,9 @@ app.listen(port, () => console.log("Corriendo servidor"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "views/home.html"));
 });
-
+app.get('/login',(req,res) => {
+  res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
 app.get('/carrito', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
