b20c81c7a23124d079a86d8144bd6b7b9b2f2b01
MaxiDeV1 <maximovelasquez411@gmail.com>
2023-02-23 03:41:54 -0300

    cambio en el app.js

--- Sprint 2/app.js
+++ Sprint 2/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.port || 8080;
+const port = process.env.port || 3001;
 const mainRuta = require('./routers/main.js')
 
 // declaracion y uso de recursos de public
