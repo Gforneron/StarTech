67c6d03f3331e03581c1124ac3b9b98ec7969db8
MaxiDeV1 <maximovelasque411@gmail.com>
2023-03-04 07:43:56 -0300

    Agregando Nueva Ruta

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -16,5 +16,8 @@ const mainController = {
     login: (req, res) => {
         res.render(path.resolve(__dirname, "../views/login.ejs"));
     },
+    form: (req,res) => {
+        res.render(path.resolve(__dirname,"../views/form-edit.ejs"));
+    }
 };
 module.exports = mainController;
