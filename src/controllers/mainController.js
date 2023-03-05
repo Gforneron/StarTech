edd038b658a5cbda7ce74f223e27b3737636f54c
Gforneron <gonzaloforneron12@gmail.com>
2023-03-05 04:19:55 -0300

    Hice articulos ejs, reformas en home y acortar archivos de controllers

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,23 +1,86 @@
 const path = require('path');
+const mainController = {}
+let productos_1 = [];
+let productos_2 = [];
 
-const mainController = {};
+productos_1.push({
+    id: 1,
+    nombre: "Celular Motorola",
+    precio: "129.999",
+    descuento: "13",
+    imagen: "Celular-Motorola.jpg",
+    clase: "cafetera"
+},
+{
+    id: 2,
+    nombre: "Celular Samgung",
+    precio: " 49.999",
+    descuento: "10",
+    imagen: "Celular-Samsung.jpg",
+    clase: "computadora"
+},
+{
+    id: 3,
+    nombre: "Notebook Asus",
+    precio: "180.777",
+    descuento: "13",
+    imagen: "Notebook-Asus.jpg",
+    clase: "telefono"
+},
+{
+    id: 4,
+    nombre: "Notebook Noblex",
+    precio: "148.000",
+    descuento: "30",
+    imagen: "Notebook-Noblex.jpg ",
+    clase: "televisor"
+})
+
+productos_2.push({
+    id: 5,
+    nombre: "Placa de Video",
+    precio: "120.000",
+    descuento: "39",
+    imagen: "Placa-Video.jpg",
+},
+{
+    id: 6,
+    nombre: "Smart TV LED",
+    precio: "270.000",
+    descuento: "27",
+    imagen: "Smart-TV-LED.jpg",
+},
+{
+    id: 7,
+    nombre: "smart TV Samsung",
+    precio: "99.999",
+    descuento: "15",
+    imagen: "Smart-TV.jpg",
+},
+{
+    id: 8,
+    nombre: "Tablet Lenovo",
+    precio: "78.976",
+    descuento: "15",
+    imagen: "Tablet-Lenovo.jpg",
+})
 
 mainController.home = (req, res) => {
-        return res.render("home");
-    }
+    return res.render ( "home.ejs", { productos_1, productos_2 });
+}
 mainController.detalles = (req, res) => {
-        return res.render("detalle-producto");
-    }
+    return res.render ( "detalle-producto" )
+}
 mainController.carrito = (req, res) => {
-        return res.render("productCart");
-    }
+    return res.render ("productCart")
+}
 mainController.register = (req, res) => {
-        return res.render ("register");
-    }
+    return res.render ("register")
+}
 mainController.login = (req, res) => {
-        return res.render ("login");
-    }
+       return res.render("login")
+}
 mainController.form = (req,res) => {
-        return res.render("form-edit")
-    }
+    return res.render("form-edit")
+}
 module.exports = mainController;
--- src/routers/main.js
+++ src/routers/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mainController = require('../controllers/mainController.js');
 
 router.get('/', mainController.home)
-router.get('/detalles', mainController.detalles)
+router.get('/detalles/:id', mainController.detalles)
 router.get('/carrito', mainController.carrito)
 router.get('/register', mainController.register)
 router.get('/login', mainController.login)
