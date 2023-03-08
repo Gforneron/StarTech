275fb90e085ef050392441c7e7eddaa84053b35f
Gforneron <gonzaloforneron12@gmail.com>
2023-03-08 04:02:01 -0300

    cambios estilos lista de productos

--- src/routers/main.js
+++ src/routers/main.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mainController = require('../controllers/mainController.js');
+const mainController = require("../controllers/mainController.js");
 
-router.get('/', mainController.home)
-router.get('/detalles/:id', mainController.detalles)
-router.get('/carrito', mainController.carrito)
-router.get('/register', mainController.register)
-router.get('/login', mainController.login)
-router.get('/form-edit',mainController.form)
-router.get('/form-charge',mainController.charge)
-router.get('/listado',mainController.listado)
+router.get("/", mainController.home);
+router.get("/detalles/:id", mainController.detalles);
+router.get("/carrito", mainController.carrito);
+router.get("/register", mainController.register);
+router.get("/login", mainController.login);
+router.get("/form-edit", mainController.form);
+router.get("/form-charge", mainController.charge);
+router.get("/listado", mainController.listado);
 module.exports = router;
