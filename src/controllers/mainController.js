e8c22ba03c213521aee8d7181f7dddf18b5df072
MaxiDeV1 <maximovelasquez411@gmail.com>
2023-03-17 05:39:17 -0300

    Cambios en el controlador

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,7 @@
 const mainController = {};
 const fs = require('fs');
-const datos = fs.readFileSync('../database/datos.json','utf-8');
-JSON.parse(datos);
+const productos = fs.readFileSync('../database/datos.json','utf-8');
+JSON.parse(productos);
 
 mainController.home = (req, res) => {
   return res.render("home.ejs", { productos });
