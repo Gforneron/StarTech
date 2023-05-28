6b30556031afa2ffe7f5d6dc4c7b4b1b7607931b
MaxiDeV1 <maximovelasquez411@gmail.com>
2023-05-28 19:06:26 -0300

    subida de validaciones

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,21 +1,35 @@
-const { check,body } = require('express-validator')
+const { body } = require("express-validator");
 const register = [
-    body('username','Ingrese un nombre de usuario')
-    .isLength({min: 5, max:8}).withMessage('Debe tener por lo menos 5 caracteres').bail(),
-    body('password','Debe ingresar una contraseña').isLength({max:5,min:8})
-    .withMessage('Este campo debe tener minimo 5 caracteres').bail()
-    .isLength({min: 5,max: 8})
-    .withMessage('Debe tener entre 5 y 8 caracteres').bail(),
-    body('confirmed','Debe confirmar la contrasña ')
-    .notEmpty().withMessage('Tienes que confirmar tu contraseña').bail(),
-    body('email')
-    .notEmpty().withMessage('Este campo es nesesario').bail(),
-    body('perfil').notEmpty().withMessage('este campo es nesesario').bail()
-]    
-const login = [
-    check('username').notEmpty().withMessage('Tienes que completar el nombre de usuario').bail(),
-    check('password').notEmpty().withMessage('Tienes que ingresar tu contraseña').bail()
-]
-
-module.exports = register
-module.exports = login
+  body("username")
+    .notEmpty()
+    .withMessage("Tienes que tener un nombre de usuario")
+    .isLength({ min: 2, max: 8 })
+    .withMessage("Debe tener por lo menos 2 caracteres")
+    .bail(),
+  body("password")
+    .notEmpty()
+    .withMessage("Debe ingresar una contraseña")
+    .isLength({ max: 5, min: 8 })
+    .withMessage("La contraseña debe tener por lo menos 8 caracteres")
+    .bail()
+    .isLength({ min: 5, max: 8 })
+    .withMessage("Debe tener entre 5 y 8 caracteres")
+    .bail(),
+  body("confirmed")
+    .notEmpty()
+    .withMessage("Tienes que confirmar tu contraseña")
+    .bail(),
+  body("email")
+    .notEmpty()
+    .withMessage("Nesecitas un correo")
+    .isEmail()
+    .withMessage("El email no es valido")
+    .bail(),
+  body("perfil").custom((value, { req }) => {
+    const extend = ['.jpeg','.png','.gif' || '.JPEG','.PNG','.GIF']
+    if (req.file === "undefined" || !req.file) {
+      throw new Error("Nesecita una foto de perfil");
+    }
+  }),
+];
+module.exports = register;
