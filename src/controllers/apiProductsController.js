03f5ab6b97551411a220d01d78e3e987aafa3532
fran-q <fdquiroga2017@gmail.com>
2023-06-11 19:07:20 -0300

    controler api productos

--- src/controllers/apiProductsController.js
+++ src/controllers/apiProductsController.js
@@ -1,31 +1,28 @@
+const { where } = require("sequelize");
 const db = require("../database/models");
 module.exports = {
   list: async (req, res) => {
-    let list = await db.Producto.findAll();
+    let listProduct = await db.Producto.findAll();
+    //delete list.imagen;
+    let listCategory = await db.Clase.findAll();
+    let searchCategory = await db.Producto.findAll({where: {clase_id: 1}});
+
+    listCategory.nameCategory = searchCategory;
+
     return res.status(200).json({
-      total: list.length,
-      lista: list,
-      status: 200,
+      count: listProduct.length,
+      countByCategory: listCategory,
+
+      category: cantCategory.length,
+
+      
     });
   },
+
   search: async (req, res) => {
     const producto = await db.Producto.findByPk(req.params.id);
     return res.status(200).json({
       producto: producto,
     });
-  },
-  create: async (req, res) => {
-    const newProduct = await db.Producto.create(req.body);
-    return res.status(200).json({
-      producto: newProduct,
-      status: 200,
-      creado: "ok",
-    });
-  },
-  delete: async (req, res) => {
-    await db.Producto.destroy({ where: { id: req.params.id } });
-    return res.status(200).json({
-      eliminado: true,
-    });
-  },
+  }
 };
