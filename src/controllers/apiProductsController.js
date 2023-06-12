2c9993fb0d2a4eaa631dd0af9ca10ecf34565927
fran-q <fdquiroga2017@gmail.com>
2023-06-12 02:09:48 -0300

    implementacion de la funcion 'list' dentro del controller de productos para el api

--- src/controllers/apiProductsController.js
+++ src/controllers/apiProductsController.js
@@ -1,21 +1,35 @@
-const { where } = require("sequelize");
 const db = require("../database/models");
 module.exports = {
   list: async (req, res) => {
-    let listProduct = await db.Producto.findAll();
-    //delete list.imagen;
-    let listCategory = await db.Clase.findAll();
-    let searchCategory = await db.Producto.findAll({where: {clase_id: 1}});
+    let listProduct = await db.Producto.findAll(req.body, {
+      attributes: ["id","nombre","clase_id"]
+    });
+    listProductsDetails = listProduct.map((product) => ({
+      id: product.id,
+      name: product.nombre,
+      description: "",
+      category:product.clase_id,
+      detail: `/api/productos/${product.id}`
+    }));
 
-    listCategory.nameCategory = searchCategory;
+    let listCategory = await db.Clase.findAll(req.body, {
+      attributes: ["id","nombre"]
+    });
+    let listCategoryDetails = {};
+    listCategory.map((category) => {
+      let categoryId = category.id;
+      let categoryName = category.nombre;
+      let categoryAmount = listProduct.filter(
+        (product) => product.clase_id == categoryId
+      ).length;
 
+      listCategoryDetails[categoryName] = categoryAmount;
+    });
+    
     return res.status(200).json({
-      count: listProduct.length,
-      countByCategory: listCategory,
-
-      category: cantCategory.length,
-
-      
+      count: listProductsDetails.length,
+      countByCategory: listCategoryDetails,
+      products:listProductsDetails,
     });
   },
 
